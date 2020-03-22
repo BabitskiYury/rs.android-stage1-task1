b5f4877e8e3d5ed1df552b1bc937a214f869cf29
Yura <babitskiy.yura@gmail.com>
2020-03-23 02:48:30 +0300

    Revert "task1"
    This reverts commit 48bfd7f9305c510e9a8c48bd663f6d5f188cf383.

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,10 +4,6 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        input.sort()
-
-        val sum = input.sum()
-
-        return intArrayOf(sum-input[input.size - 1], sum-input[0])
+        throw NotImplementedError("Not implemented")
     }
 }
--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,11 +5,6 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        val sum = (bill.sum() - bill[k])/2
-
-        if(sum!=b)
-            return (b-sum).toString()
-        else
-            return "Bon Appetit"
+        throw NotImplementedError("Not implemented")
     }
 }
